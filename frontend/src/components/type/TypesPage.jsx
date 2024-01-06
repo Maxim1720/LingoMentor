c838c4304181dac7fc552574a76422dd344f2921
maksim1708 <musiev_maksim@mail.ru>
2024-01-06 14:16:21 +0500

    To make TypesPage func component

--- frontend/src/components/type/TypesPage.jsx
+++ frontend/src/components/type/TypesPage.jsx
@@ -1,97 +1,105 @@
-import {Alert, Typography} from "@mui/material";
+import { Alert, Typography } from "@mui/material";
 import EntityPage from "../EntityPage";
 import ItemCard from "../ItemCard";
 import TypeForm from "./TypeForm";
+import { useCallback, useEffect, useState } from "react";
 
 export default function TypesPage() {
-    const apiUrl = import.meta.env.VITE_API_TYPES;
+  const apiUrl = import.meta.env.VITE_API_TYPES;
+  const [itemsState, setItemsState] = useState([]);
 
-    const loadItems = async () => {
-        const resp = await fetch(apiUrl);
-        const json = await resp.json();
-        // console.log(json);
-        return await json._embedded.lessonTypes;
-    };
+  const loadItems = useCallback(async () => {
+    const resp = await fetch(apiUrl);
+    const json = await resp.json();
+    // console.log(json);
+    return await json._embedded.lessonTypes;
+  }, [apiUrl]);
 
-    return (
-        <EntityPage
-            loadItems={loadItems}
-            renderItems={(items) => {
-                console.log(items);
+  const updateItemsState = useCallback(() => {
+    loadItems().then(items=>setItemsState([...items]));
+  }, [loadItems]);  
 
-                const listOfItems = [];
 
-                items.map((i) => {
-                    listOfItems.push(
-                        <ItemCard
-                            item={i}
-                            key={i._links.self.href}
-                            onEdit={() => (items = loadItems())}
-                            card={() => {
-                                return (
-                                    <Typography>{i.name}</Typography>
-                                    // <Card>
-                                    //   <CardHeader title={i.name}/>
-                                    // </Card>
-                                );
-                            }}
-                            form={({onEdit}) => {
-                                console.log("form now");
-                                return (
-                                    <TypeForm
-                                        onSubmit={(data) => {
-                                            fetch(data._links.self.href, {
-                                                method: "PATCH",
-                                                headers: {
-                                                    "Content-Type": "application/json",
-                                                    Accept: "application/json",
-                                                },
-                                                body: JSON.stringify(data),
-                                            })
-                                                .then((resp) => resp.json())
-                                                .then((resp) => {
-                                                    console.log(resp);
-                                                    // setEditing(!isEditing);
-                                                    onEdit();
-                                                })
-                                                .catch((e) => {
-                                                    return <Alert severity={"danger"}>{e.message}</Alert>;
-                                                });
-                                        }}
-                                        initData={i}
-                                    />
-                                );
-                            }}
-                        />
-                    );
-                });
+  useEffect(() => {updateItemsState()}, [updateItemsState]);
 
-                return listOfItems;
-            }}
-            renderForm={(onSubmit, onError) => {
+  return (
+    <EntityPage
+      loadItems={async () => itemsState}
+      renderItems={(items) => {
+        console.log(items);
+
+        const listOfItems = [];
+
+        items.map((i) => {
+          listOfItems.push(
+            <ItemCard
+              item={i}
+              key={i._links.self.href}
+              onEdit={updateItemsState}
+              card={() => {
                 return (
-                    <TypeForm
-                        onSubmit={(data) => {
-                            fetch(import.meta.env.VITE_API_TYPES, {
-                                method: "POST",
-                                headers: {
-                                    "Content-Type": "application/json",
-                                },
-                                body: JSON.stringify(data),
-                            })
-                                .then((resp) => {
-                                    console.log(resp);
-                                    //   loadItems();
-                                    onSubmit(resp);
-                                })
-                                .catch((error) => {
-                                    //   setData({ error: error });
-                                    onError(error);
-                                });
-                        }}
-                    />
+                  <Typography>{i.name}</Typography>
                 );
+              }}
+              form={(closeForm) => {
+                console.log("form now");
+                return (
+                  <TypeForm
+                    onSubmit={(data) => {
+                      fetch(data._links.self.href, {
+                        method: "PATCH",
+                        headers: {
+                          "Content-Type": "application/json",
+                          Accept: "application/json",
+                        },
+                        body: JSON.stringify(data),
+                      })
+                        .then((resp) => resp.json())
+                        .then((resp) => {
+                          console.log(resp);
+                          closeForm();
+                          updateItemsState();
+                        })
+                        .catch((e) => {
+                          return <Alert severity={"danger"}>{e.message}</Alert>;
+                        });
+                    }}
+                    initData={i}
+                  />
+                );
+              }}
+              onDelete={updateItemsState}
+            />
+          );
+        });
+
+        return listOfItems;
+      }}
+      renderForm={(onSubmit, onError) => {
+        return (
+          <TypeForm
+            onSubmit={(data) => {
+              fetch(import.meta.env.VITE_API_TYPES, {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+              })
+                .then((resp) => {
+                  console.log(resp);
+                  //   loadItems();
+                  onSubmit(resp);
+                  updateItemsState();
+                })
+                .catch((error) => {
+                  //   setData({ error: error });
+                  onError(error);
+                });
             }}
-        />
-    );
+          />
+        );
+      }}
+    />
+  );
 }
